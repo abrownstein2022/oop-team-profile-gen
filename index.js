6ec4c452bb06d1a08d0f48c8bda27eb822bb2cf8
Alexis Brownstein <alexis@drdatabase.com>
2022-12-28 15:17:54 -0500

    forgot to fix font awesome link to older version to get icons I need

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const generateHTML = () => {
        `; 
     }     
   });
-
+// had to use older version of font awesome to get all the icons I needed
   let htmlString = `<!DOCTYPE html>
    <html lang="en">
    <head>
@@ -71,7 +71,7 @@ const generateHTML = () => {
    <meta charset="UTF-8" />
    <meta name="viewport" content="width=device-width, initial-scale=1" />
    <link rel="stylesheet" type="text/css" href="../src/css/style.css" />
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">  
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">  
  </head>
 
  <body>
